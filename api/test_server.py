dd49708d807ac16e7f40e76127991687e61e7c13
Jorge Dominguez <jorgedc93@gmail.com>
2016-04-28 19:07:09 +0200

    Added tests for the POST

--- api/test_server.py
+++ api/test_server.py
@@ -50,6 +50,44 @@ class ServerTestCase(unittest.TestCase):
         self.assertIn("response", response_dict)
         self.assertEqual(response_dict["response"], "No users available")
 
+    def test_post_create_user(self):
+        """ Test that a successful post creates a user """
+        random_user = generate_random_user()
+
+        with patch.object(server, "create_new_user", return_value=(True, str(random_user))):
+            response = self.app.post(URL_BASE + "users/", data=ujson.dumps(random_user),
+                                     content_type='application/json')
+
+        response_dict = ujson.loads(response.data.decode("utf-8"))
+
+        self.assertIn("status", response_dict)
+        self.assertEqual(response_dict["status"], "ok")
+
+        self.assertIn("data", response_dict)
+        self.assertEqual(response_dict["data"], str(random_user))
+
+    def test_wrong_post_failure(self):
+        """ Test that a wrong post fails creating a user """
+        random_user = generate_random_user()
+        err_msg = "I'm an error message"
+
+        with patch.object(server, "create_new_user", return_value=(False, err_msg)):
+            response = self.app.post(URL_BASE + "users/", data=ujson.dumps(random_user),
+                                     content_type='application/json')
+
+        response_dict = ujson.loads(response.data.decode("utf-8"))
+
+        self.assertIn("status", response_dict)
+        self.assertEqual(response_dict["status"], "error")
+
+        self.assertIn("error", response_dict)
+        self.assertEqual(response_dict["error"], err_msg)
+
+        response_msg = "Unable to create user with data: {}".format(random_user)
+        self.assertIn("response", response_dict)
+        self.assertEqual(response_dict["response"], response_msg)
+
+
 
 if __name__ == '__main__':
     unittest.main()
