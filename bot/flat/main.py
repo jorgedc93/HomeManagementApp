4ddff4df6497cb57645fbf887db454090fde9ef3
Michael Petretti <michael.petretti@skyscanner.net>
2016-05-20 21:20:10 +0200

    first api trys

--- bot/flat/main.py
+++ bot/flat/main.py
@@ -4,9 +4,13 @@
 
 import telepot
 import time
+import requests
+import json
 
+from  flat.members import Member
 from flat.config import bot_cfg, procenca56
 
+ENDPOINT = "http://192.168.1.232:5000/api/v1/users/"
 
 def run():
     bot = telepot.Bot(bot_cfg["token"])
@@ -17,12 +21,54 @@ def run():
 def react_on_message(message):
     content_type, chat_type, chat_id = telepot.glance(message)
     if not_authorised(content_type, chat_id):
-        text = message["text"]
-        telepot.Bot(bot_cfg["token"]).sendMessage(chat_id, "What do you mean with " + text + " ?")
-        
+        if put_money(message["text"]):
+            print(message["text"])
+            _ , amount = message["text"].split(" ")
+            print(_)
+            print()
+            print(amount)
+            print()
+            amount = int(amount)
+            update_total(amount, chat_id)
+        member_list = get_members()
+        telepot.Bot(bot_cfg["token"]).sendMessage(chat_id, str(member_list))
+
 
 def not_authorised(content_type, chat_id):
         return content_type == "text" and chat_id in procenca56.keys()
+
+
+def get_members():
+    user_list = get_users_from_api_endpoint()
+    return [Member(user["username"], user["total"]).to_dict() for user in user_list]
+
+
+def get_users_from_api_endpoint():
+    response = requests.get(ENDPOINT)
+    return response.json()['data']
+
+
+def update_total(amount, chat_id):
+    url = ENDPOINT + procenca56[chat_id] + "/"
+    response = requests.get(url)
+    user = response.json()['data']
+    print()
+    print(str(user))
+    print()
+    user["total"] += amount
+    print()
+    print(str(user))
+    print()
+    data = {"total": user["total"]}
+    response = requests.put(url, data=json.dumps(data))
+    print()
+    print(str(response))
+    print()
+
+
+def put_money(text):
+    command, amount = text.split()
+    return command == "put"
 
 
 if __name__ == '__main__':
@@ -30,3 +76,17 @@ if __name__ == '__main__':
     # Keep the program running.
     while 1:
         time.sleep(10)
+
+
+def get_usernames():
+    user_dict =  get_users_from_api_endpoint()
+    return list_of("username", user_dict)
+
+
+def get_users_total():
+    user_dict = get_users_from_api_endpoint()
+    return list_of("total", user_dict)
+
+
+def list_of(key, dict):
+    return [user[key] for user in dict]
--- bot/flat/members.py
+++ bot/flat/members.py
@@ -2,16 +2,14 @@
 # coding=utf-8
 #
 
-provenca56 = {
-    63061755 : "michael",
-    24674958: "jorge",
-    165278994: "nacho"
-}
+class Member(object):
 
-
-class members(object):
-
-    def __init__(self, name, chat_id):
+    def __init__(self, name, total):
         self.name = name
-        self.chat_id = chat_id
+        self.total = total
 
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "total": self.total
+        }
